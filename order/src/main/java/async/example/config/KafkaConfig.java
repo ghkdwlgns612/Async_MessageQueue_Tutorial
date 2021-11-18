e7476be1e4be2b532caedabef4b9e92daf9992fa
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-11-18 23:10:51 +0900

    kafka consumer, producer configuration 수정..

--- order/src/main/java/async/example/config/KafkaConfig.java
+++ order/src/main/java/async/example/config/KafkaConfig.java
@@ -4,9 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import async.example.domain.OrderRequestDto;
+import async.example.domain.PayRequestDto;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.BytesSerializer;
+import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -14,20 +21,19 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaConfig {
-    public KafkaConfig() {
-    }
 
     @Bean
     public ProducerFactory<String, OrderRequestDto> producerFactory() {
         Map<String, Object> config = new HashMap();
-        config.put("bootstrap.servers", "127.0.0.1:9092");
-        config.put("key.serializer", StringSerializer.class);
-        config.put("value.serializer", JsonSerializer.class);
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory(config);
     }
 
     @Bean
     public KafkaTemplate<String, OrderRequestDto> kafkaTemplate() {
-        return new KafkaTemplate(this.producerFactory());
+        return new KafkaTemplate<>(producerFactory());
     }
 }
--- payment/src/main/java/async/example/config/KafkaConfig.java
+++ payment/src/main/java/async/example/config/KafkaConfig.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import async.example.domain.AsyncRequest;
+import async.example.domain.PayRequestDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,19 +17,21 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 @Configuration
 public class KafkaConfig {
     @Bean
-    public ConsumerFactory<String, AsyncRequest> consumerFactory() {
+    public ConsumerFactory<String, PayRequestDto> consumerFactory() {
         Map<String, Object> config = new HashMap();
-        config.put("bootstrap.servers", "127.0.0.1:9092");
-        config.put("group.id", "group_id");
-        config.put("key.deserializer", StringDeserializer.class);
-        config.put("value.deserializer", JsonDeserializer.class);
-        return new DefaultKafkaConsumerFactory(config);
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        return new DefaultKafkaConsumerFactory(config, new StringDeserializer(), new JsonDeserializer(PayRequestDto.class, false));
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, AsyncRequest> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, AsyncRequest> factory = new ConcurrentKafkaListenerContainerFactory();
-        factory.setConsumerFactory(this.consumerFactory());
+    public ConcurrentKafkaListenerContainerFactory<String, PayRequestDto> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, PayRequestDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
         return factory;
     }
 }
